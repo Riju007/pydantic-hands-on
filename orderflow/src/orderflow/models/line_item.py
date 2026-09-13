3b6b1f8bb6bc29564f0cf8ed5707d909be88e40d
AVISEK DEY <avisekdey1994@gmail.com>
2026-09-13 17:55:10 +0530

    Created order model

--- orderflow/src/orderflow/models/line_item.py
+++ orderflow/src/orderflow/models/line_item.py
@@ -10,7 +10,7 @@ how many, and at what price. Remember: money should be Decimal, not float.
 from uuid import uuid4
 from typing import Literal
 from decimal import Decimal
-
+from datetime import datetime, timezone
 
 from pydantic import BaseModel, UUID4, Field, PositiveInt
 
@@ -33,4 +33,42 @@ class LineItem(BaseModel):
     )
     category: str | None = Field(
         description="category of the product", repr=False, default=None
+    )
+
+
+ORDER_STATUS = Literal[
+    "pending",
+    "delivered",
+    "returned",
+    "cancelled",
+    "shipped",
+    "out_for_delivery",
+]
+
+PAYMENT_STATUS = Literal[
+    "paid",
+    "cash_on_delivery",
+    "refund",
+]
+
+
+class Order(BaseModel):
+    """Order model"""
+
+    order_id: UUID4 = Field(default_factory=uuid4, description="Unique order id")
+    customer_name: str = Field(
+        ..., max_length=100, description="Full name of the customer"
+    )
+    line_items: list[LineItem] = Field(
+        default_factory=list, description="Items in the order"
+    )
+    status: ORDER_STATUS = Field(default="pending", description="Order status")
+    payment_status: PAYMENT_STATUS = Field(..., description="Status of the payment")
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        description="Order created at",
+    )
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        description="Order updated at",
     )
